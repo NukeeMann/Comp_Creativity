1743143a6dd9e0f9e57aed93211991ee933b0453
Jacek B <jaceble@interia.eu>
2021-03-12 21:50:46 +0100

    minor updates XD

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import librosa
 import IPython.display as ipd
 
-x, sr = librosa.load("music/eminem2.wav")
+x, sr = librosa.load("music/eminem.wav")
 ipd.Audio(x, rate=sr)
 tempo, beat_times = librosa.beat.beat_track(x, sr=sr, start_bpm=60, units='time')
 clicks = librosa.clicks(beat_times, sr=sr, length=len(x))
