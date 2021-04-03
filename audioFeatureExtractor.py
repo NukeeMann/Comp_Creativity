a2b4d3a17d3b0f13e48ac35cf8a6ec9a4a129220
Michał Wnuczyński <49735204+NukeeMann@users.noreply.github.com>
2021-04-03 22:10:23 +0200

    Added rhythm extractor and save file option

--- audioFeatureExtractor.py
+++ audioFeatureExtractor.py
@@ -1,44 +1,66 @@
 import matplotlib.pyplot as plt
 import librosa.display
+import soundfile as sf
 
 
 class AudioFtExt:
-    audio_path = ""
-    hz_scale = 0
-    start_time = 0
-    duration_time = 0
-    audio_data = None
-    spec_data = None
+    audio_path = ""  # Path to audio file
+    sampling_rate = 0  # Hz upper limit
+    start_time = 0  # Number of seconds skipped from the beginning of audio during loading
+    duration_time = 0  # Number of seconds loaded from audio
+    tempo = 0  # Estimated global tempo in beats per min
+    audio_data = None  # Audio data. Loudness of audio in specified time location
+    spec_data = None  # Spectrogram data. Loudness of audio in specified frequency frame. (dB)
+    beat_data = None  # Beats data. Number of beat and their time location
 
     def __init__(self, audio_path, hz_scale=40000, start_time=0.0, duration_time=None):
         self.audio_path = audio_path
-        self.hz_scale = hz_scale
+        self.sampling_rate = hz_scale
         self.start_time = start_time
         self.duration_time = duration_time
 
+    # Loads audio file
     def convertAudioToData(self):
         if self.duration_time is None:
-            self.audio_data, self.hz_scale = librosa.load(self.audio_path, sr=self.hz_scale, offset=self.start_time)
-            self.duration_time = self.audio_data / self.hz_scale
+            self.audio_data, self.sampling_rate = librosa.load(self.audio_path, sr=self.sampling_rate,
+                                                               offset=self.start_time)
+            self.duration_time = self.audio_data / self.sampling_rate
         else:
-            self.audio_data, self.hz_scale = librosa.load(self.audio_path, sr=self.hz_scale, offset=self.start_time,
-                                                          duration=self.duration_time)
+            self.audio_data, self.sampling_rate = librosa.load(self.audio_path, sr=self.sampling_rate,
+                                                               offset=self.start_time, duration=self.duration_time)
 
+    # Creates spectrogram data
     def getSpectrogramData(self):
         x = librosa.stft(self.audio_data)
         self.spec_data = librosa.amplitude_to_db(abs(x))
 
+    # Extracts beat moments
+    # ARG 1: Guessed tempo. For better prediction
+    def getRhythmData(self, beats_per_min=60):
+        self.tempo, self.beat_data = librosa.beat.beat_track(self.audio_data, sr=self.sampling_rate,
+                                                             start_bpm=beats_per_min, units='time')
+
+    # Need to perform getSpectrogramData() and convertAudioToData() first
     def plotSpectrogram(self):
         plt.figure(figsize=(14, 5))
-        librosa.display.specshow(self.spec_data, sr=self.hz_scale, x_axis='time', y_axis='hz')
+        librosa.display.specshow(self.spec_data, sr=self.sampling_rate, x_axis='time', y_axis='hz')
         plt.colorbar()
         plt.show()
 
-    def plotWaveplot(self):
+    # Need to perform convertAudioToData() first
+    def plotWave(self):
         plt.figure(figsize=(14, 5))
-        print(type(self.audio_data), type(self.hz_scale))
-        librosa.display.waveplot(self.audio_data, sr=self.hz_scale)
+        print(type(self.audio_data), type(self.sampling_rate))
+        librosa.display.waveplot(self.audio_data, sr=self.sampling_rate)
         plt.show()
+
+    # Need to perform convertAudioToData() first and getRhythmData() if with_clicks = True
+    def saveAudio(self, with_clicks=False, name='test'):
+        if with_clicks:
+            clicks = librosa.clicks(self.beat_data, sr=self.sampling_rate, length=len(self.audio_data))
+            sf.write(name + '.wav', self.audio_data + clicks, self.sampling_rate)
+        else:
+            sf.write(name + '.wav', self.audio_data, self.sampling_rate)
 
     def samplesPerFrame(self, fps=30):
         # TODO
--- main.py
+++ main.py
@@ -3,19 +3,22 @@ from audioFeatureExtractor import AudioFtExt
 
 def main():
     # Define audios path
-    audio_path = 'SampleAudio/HzTest.wav'
+    audio_path = 'SampleAudio/Eminem_the_dfiff.wav'
 
     # Declare and prepare Audio Feature Extractor class
     afe = AudioFtExt(audio_path)
     # Convert audio to data format
     afe.convertAudioToData()
     # Show plot of loudness of audio
-    afe.plotWaveplot()
+    afe.plotWave()
     # Convert audios data to spectrogram data using Fourier
     afe.getSpectrogramData()
     # Show plot of dB per Hz per second
     afe.plotSpectrogram()
-    afe.samplesPerFrame()
+    # Detects beats momentum
+    afe.getRhythmData()
+    # Saves data to .wav file with CLICK sound at detected beats momentum
+    afe.saveAudio(True)
 
 
 if __name__ == "__main__":
