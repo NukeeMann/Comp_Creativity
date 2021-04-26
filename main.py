67ac69cf1d7ade62ba05d583df027001e9a8924e
Jacek B <jaceble@interia.eu>
2021-04-26 19:17:03 +0200

    generation of video with various color

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import numpy as np
 audio_file = 'music/whitney.wav'
 image_folder = 'images'
 video_name = 'video.avi'
+
 # default value is 22050. with different value beat times might not be accurate
 afe = AudioFtExt(audio_file, hz_scale=22050)
 afe.getSpectrogramData()
 afe.getRhythmData(60)
-# get rhythm from track
 beat_times = afe.beat_data
 
 # ## functions that convert spectrogram to one value per one moment in time to easily use it in image procesing;
@@ -21,8 +21,10 @@ spec_data = afe.spec_data.T
 indexes = []
 arrSize = int(len(spec_data[0])/3)
 for i in range(len(spec_data)):
-    indexes.append(np.mean(spec_data[i][0:arrSize]))
+    indexes.append([np.mean(spec_data[i][0:arrSize]),np.mean(spec_data[i][arrSize:2*arrSize]),np.mean(spec_data[i][2*arrSize:])])
 max_db = max(indexes)
+min_db = min(indexes)
+tmp_db = [(max_db[0] - min_db[0]),(max_db[1] - min_db[1]),(max_db[2] - min_db[2])]
 song_time = int(max(beat_times) * 100)
 print(song_time)
 # create list of images and single frame (all images must be the same size to fit into created frame)
@@ -34,16 +36,16 @@ video = cv2.VideoWriter(video_name, 0, 100, (width, height))
 # attach images to frames
 i = 0.01
 frame_number = 0
-# TODO: delete this, created temporary for visual purposes
-indexes = np.array(indexes) / 3
 for image in images:
     while i < beat_times[frame_number]:
         tmp = i * 100 * len(indexes) / song_time
         current_index = int(tmp)  # actual index from flatten spectrogram array
-        val = indexes[current_index] / max_db * 100
-        print(val)
-        adjusted = cv2.convertScaleAbs(cv2.imread(os.path.join(image_folder, image)), alpha=1, beta=val)
-        video.write(adjusted)
+        rgb = indexes[current_index]
+        rgb = [((rgb[0]-min_db[0])/tmp_db[0]) * 255,((rgb[1]-min_db[1])/tmp_db[1]) * 255,((rgb[2]-min_db[2])/tmp_db[2]) * 255]
+        rgbImage = np.zeros((frame.shape), np.uint8)
+        # rgbImage[::] = (int(rgb[0]), int(rgb[1]), int(rgb[2]))
+        rgbImage[::] = (100, 100, int(rgb[2]))
+        video.write(rgbImage)
         i = i + 0.01
     frame_number = frame_number + 1
     if frame_number >= len(beat_times):
