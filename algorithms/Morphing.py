209d59287226ad883be292785a33ff6cdc9f1c99
Jacek B <jaceble@interia.eu>
2021-07-21 23:16:31 +0200

    Loading of morphing model moved to seperate thread. Output video save created.

--- algorithms/Morphing.py
+++ algorithms/Morphing.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 import cv2
@@ -7,7 +8,23 @@ import moviepy.editor as mpe
 import numpy as np
 from scipy.stats import truncnorm
 import tensorflow_hub as hub
+from tkinter.messagebox import showerror
+from threading import Thread
+from concurrent.futures import Future
 
+def call_with_future(fn, future, args, kwargs):
+    try:
+        result = fn(*args, **kwargs)
+        future.set_result(result)
+    except Exception as exc:
+        future.set_exception(exc)
+
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        future = Future()
+        Thread(target=call_with_future, args=(fn, future, args, kwargs)).start()
+        return future
+    return wrapper
 
 class Morphing(tk.Frame):
     num_samples = 1
@@ -19,17 +36,64 @@ class Morphing(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         top_padding = 50
-        self.x1 = tk.Entry(self, bd=5, font=44, justify='center')
-        self.x1.place(x=460, y=top_padding + 20, height=40, width=400)
-        self.x2 = tk.Entry(self, bd=5, font=44, justify='center')
-        self.x2.place(x=460, y=top_padding + 61, height=40, width=400)
+        self.video = None
+        self.label_select_styl = tk.Label(self, text='Choose images to create morphing: ', font=("TkDefaultFont", 16))
+        self.label_select_styl.place(x=60, y=top_padding-10, height=30, width=350)
+        self.choices = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.image_number_1 = tk.IntVar(self)
+        self.image_number_1.set(self.choices[0])
+
+        self.choose_box_param1 = tk.OptionMenu(self, self.image_number_1, *self.choices)
+        self.choose_box_param1.config(font=("TkDefaultFont", 12))
+        dropdown1 = self.nametowidget(self.choose_box_param1.menuname).config(font=("TkDefaultFont", 12))
+        self.choose_box_param1.place(x=460, y=top_padding + 20, height=40, width=400)
+
+        self.image_number_2 = tk.IntVar(self)
+        self.image_number_2.set(self.choices[0])
+        self.choose_box_param2 = tk.OptionMenu(self, self.image_number_2, *self.choices)
+        self.choose_box_param2.config(font=("TkDefaultFont", 12))
+        dropdown2 = self.nametowidget(self.choose_box_param2.menuname).config(font=("TkDefaultFont", 12))
+        self.choose_box_param2.place(x=460, y=top_padding + 61, height=40, width=400)
 
         self.label1 = tk.Label(self, text='Img number:', font=44, background="lightgrey").place(x=60, y=top_padding + 20, height=40, width=400)
         self.label2 = tk.Label(self, text='Img number:', font=44, background="lightgrey").place(
             x=60, y=top_padding + 61, height=40, width=400)
-        self.button3 = tk.Button(self, text='Generate!', font=44, command=self.create_morphing).place(
-                                        x=60, y=top_padding + 102, height=40, width=800)
+        self.generate_button = tk.Button(self, text='Generate!', font=44, command=self.create_morphing).place(
+                                        x=60, y=top_padding + 102, height=40, width=400)
+        self.save_button = tk.Button(self, text='Save!', font=44, command=self.save_morphing).place(
+                                        x=460, y=top_padding + 102, height=40, width=400)
+        # Load the model
+        self.load_model_h = self.loadModel()
 
+    @threaded
+    def loadModel(self):
+        self.module_path = "https://tfhub.dev/deepmind/biggan-deep-128/1"   # 256 doesn't work  for some reason
+        tf.reset_default_graph()
+        print('Loading BigGAN module from:', self.module_path)
+        self.module = hub.Module(self.module_path)
+        self.inputs = {k: tf.placeholder(v.dtype, v.get_shape().as_list(), k)
+                       for k, v in self.module.get_input_info_dict().items()}
+        self.output = self.module(self.inputs)
+        self.input_z = self.inputs['z']
+        self.input_y = self.inputs['y']
+        self.input_trunc = self.inputs['truncation']
+        self.dim_z = self.input_z.shape.as_list()[1]
+        self.vocab_size = self.input_y.shape.as_list()[1]
+        initializer = tf.global_variables_initializer()
+        self.sess = tf.Session()
+        self.sess.run(initializer)
+        print("Morphing is ready to go!!!")
+
+    def save_morphing(self):
+        if self.video is None:
+            tk.messagebox.showerror(title="Error", message="There is nothing to save. Create morphing first.")
+            return
+
+        filename = filedialog.asksaveasfile(mode='wb', defaultextension=".mp4", filetypes=(("MP4", "*.mp4"),
+                                                                                           ("all files", "*.*")))
+        if not filename:
+            return
+        self.video.write_videofile(filename.name, fps=100)
 
     def truncated_z_sample(self, batch_size, truncation=1., seed=None):
         state = None if seed is None else np.random.RandomState(seed)
@@ -90,37 +154,27 @@ class Morphing(tk.Frame):
 
     # Function creating video showing morphing between 2 photos generated by GAN
     def create_morphing(self):
-        self.module_path = 'https://tfhub.dev/deepmind/biggan-deep-256/1'
-        tf.reset_default_graph()
-        print('Loading BigGAN module from:', self.module_path)
-        self.module = hub.Module(self.module_path)
-        self.inputs = {k: tf.placeholder(v.dtype, v.get_shape().as_list(), k)
-                  for k, v in self.module.get_input_info_dict().items()}
-        self.output = self.module(self.inputs)
-        self.input_z = self.inputs['z']
-        self.input_y = self.inputs['y']
-        self.input_trunc = self.inputs['truncation']
-        self.dim_z = self.input_z.shape.as_list()[1]
-        self.vocab_size = self.input_y.shape.as_list()[1]
-        initializer = tf.global_variables_initializer()
-        sess = tf.Session()
-        sess.run(initializer)
-        category_A = int(self.x1.get())
-        category_B = int(self.x2.get())
+        if self.image_number_1.get() == '' or self.image_number_2.get() == '':
+            tk.messagebox.showerror(title="Error", message="Select image number first.")
+            return
+        # Check if model is loaded
+        self.load_model_h.result()
+        category_A = int(self.image_number_1.get())
+        category_B = int(self.image_number_2.get())
         z_A, z_B = [self.truncated_z_sample(self.num_samples, self.truncation, noise_seed)
                     for noise_seed in [self.noise_seed_A, self.noise_seed_B]]
         y_A, y_B = [self.one_hot([category] * self.num_samples, self.vocab_size)
                     for category in [category_A, category_B]]
         z_interp = self.interpolate_and_shape(z_A, z_B, self.num_interps)
         y_interp = self.interpolate_and_shape(y_A, y_B, self.num_interps)
-        ims = self.sample(sess, z_interp, y_interp, self.vocab_size, truncation=self.truncation)
+        ims = self.sample(self.sess, z_interp, y_interp, self.vocab_size, truncation=self.truncation)
         video_name = 'video.avi'
         height, width, layers = ims[0].shape
-        video = cv2.VideoWriter(video_name, 0, 50, (width, height))
+        self.video = cv2.VideoWriter(video_name, 0, 50, (width, height))
         for img in ims:
-            video.write(img)
+            self.video.write(img)
         cv2.destroyAllWindows()
-        video.release()
-        video = mpe.VideoFileClip(video_name)
-        video.write_videofile("output.mp4", fps=50)
+        self.video.release()
+        self.video = mpe.VideoFileClip(video_name)
+        self.video.write_videofile("output.mp4", fps=50)
         os.startfile("output.mp4")
