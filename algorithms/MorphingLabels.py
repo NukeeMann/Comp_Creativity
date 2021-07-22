c61eefba79ed2d417bc3c465c8706fa8465d0a79
Jacek B <jaceble@interia.eu>
2021-07-22 20:42:58 +0200

    Morphing image selection upgraded. Color Mix GUI updated

---
+++ algorithms/MorphingLabels.py
@@ -0,0 +1,19 @@
+
+labels = ["Blue fish",
+          "Shark",
+          "???",
+          "???",
+          "Płaszczka",
+          "???",
+          "Kogut",
+          "???"
+          ]
+
+
+def get_labels():
+    return labels
+
+
+def get_value(label):
+    return labels.index(label) + 1
+
